992d294fe64a5e9002fb5f8fc17cd257f1c5b97c
rukurx <rukurx@users.noreply.github.com>
2016-05-30 21:35:50 +0900

    modify: merge btn and close btn

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(function(){
-
+    // Disable a merge button and a close button if work is in process
+    var $issueTitle = $('.js-issue-title');
+    if ($issueTitle.text().match(/wip/i)) {
+        // merge button
+        $('.js-merge-branch-action').attr('disabled', true);
+        // close button
+        $('.js-comment-and-button').attr('disabled', true);
+    }
 });
